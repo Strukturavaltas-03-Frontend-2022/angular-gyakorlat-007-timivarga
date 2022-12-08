5b8aaad55be69a8acee07dffa9c95b6c83fc56a0
timivarga <timea.varga252@gmail.com>
2022-12-08 01:51:44 +0100

    ??

--- src/app/page/events-list/events-list.component.ts
+++ src/app/page/events-list/events-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Event } from 'src/app/model/event';
 import { EventService } from 'src/app/service/event.service';
@@ -14,7 +15,12 @@ export class EventsListComponent implements OnInit {
 
   constructor(
     private eventService: EventService,
+    private router: Router,
   ) { }
+
+  onClicked(event: Event): void {
+    this.router.navigate([`event/${event.id}`]);
+  }
 
   ngOnInit(): void {}
 
